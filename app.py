3a32ab90afbd497580582a60634213c24c7bf626
FauzanIlyasAlmeyda <ggid.uzen@gmail.com>
2026-09-10 20:29:53 +0700

    fix CORS

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ import uuid
 import joblib
 import pandas as pd
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 from core import descriptive_analysis, train_model
 
 app = Flask(__name__)
+CORS(app)
 
 # In-memory store: session_id -> data
 _store: dict = {}
